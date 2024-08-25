14d70baa1509f3310e520889229419a48e181dd0
Juan Manuel B P <pro18qwe@gmail.com>
2024-08-25 18:41:30 +0200

    FIX: canvasSize is assigned a value but never used  no-unused-vars

--- src/components/VisualPoetry/canvasToolbox/editor/CanvasTextEditor.jsx
+++ src/components/VisualPoetry/canvasToolbox/editor/CanvasTextEditor.jsx
@@ -31,7 +31,6 @@ import MobileEditor from './MobileEditor';
  */
 const CanvasTextEditor = ({ title, id }) => {
   const { state, actions } = useAppContext();
-  const { canvasSize } = state;
   const [isGalleryOpen, setIsGalleryOpen] = useState(false);
   const { updateInputDataAction } = actions;
   const { MOBILE, TABLET, LAPTOP } = DEVICE_TYPES;
@@ -53,7 +52,7 @@ const CanvasTextEditor = ({ title, id }) => {
    * @member module:canvasToolbox/CanvasTextEditor
    */
   const generateText = useGenerateTextLoremIpsum();
-  
+
   useEffect(() => {
     const newText = generateText();
     updateInputDataAction(state.inputData + newText);
